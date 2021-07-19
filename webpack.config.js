13875acb11a87fffe310c0e928eb3abd15b3a353
vasiliy.uskov <vasiliy.uskov@ispring.com>
2021-07-19 13:04:29 +0300

    Инициализация

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'main.out.js',
+        filename: 'min.out.js',
         path: path.resolve(__dirname, './build'),
     },
 };
